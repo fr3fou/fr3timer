f9b10dc556049b6d161d7d7ad5f118b99c343816
Simo Aleksandrov <simo3003@me.com>
2018-08-15 19:54:49 +0300

    Fix puzzle type inner html

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,11 +4,13 @@ import Scrambo from 'scrambo';
 import Solve from '../models/solve';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+
 class TopBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
             puzzleType: 333,
+            puzzleName: '3x3x3',
             scramble: ''
         }
         this.handleChange = this.handleChange.bind(this);
@@ -24,28 +26,35 @@ class TopBar extends Component {
     }
 
     handleChange(e) {
+        console.log(e);
         this.setState({
+            puzzleName: e._targetInst.stateNode.innerText,
             puzzleType: e.target.value
         }, () => {
             this.generateScramble();
-        })
+        });
     }
 
     render() {
         return (
             <div className='TopBarWrapper'>
-                {this.state.puzzleType}
+                {this.state.puzzleName}
                 <div className='TopBarScramble'>
                     {this.state.scramble}
                 </div>
-                <Select
-                    value={this.state.puzzleType}
-                    onChange={this.handleChange}
-                >
-                    <MenuItem value={222}>2x2x2</MenuItem>
-                    <MenuItem value={333}>3x3x3</MenuItem>
-                    <MenuItem value={444}>4x4x4</MenuItem>
-                </Select>
+                <div>
+                    <Select
+                        value={this.state.puzzleType}
+                        onChange={this.handleChange}
+                        inputProps={{
+                            id: 'puzzleTypeSelect',
+                        }}
+                    >
+                        <MenuItem value={222}>2x2x2</MenuItem>
+                        <MenuItem value={333}>3x3x3</MenuItem>
+                        <MenuItem value={444}>4x4x4</MenuItem>
+                    </Select>
+                </div>
             </div>
         );
     }
